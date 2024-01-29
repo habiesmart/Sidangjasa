694729b791e76d868e792965a3b73e86e2b1dbf4
muhammad.budiman <habie.budiman@360consulting.co.id>
2024-01-29 21:22:06 +0700

    add confirm sebelum hapus customer

--- resources/views/Customer/index.blade.php
+++ resources/views/Customer/index.blade.php
@@ -46,7 +46,7 @@
                                     @csrf
                                     @method('delete')
 
-                                    <button class="btn btn-danger" type="submit">Hapus</button>
+                                    <button class="btnHapus btn btn-danger" type="submit">Hapus</button>
                                 </form>
                             </td>
                         </tr> 
@@ -59,10 +59,12 @@
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         {{-- <script src="//cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script> --}}
-        {{-- <script>
+        <script>
             $(document).ready(function(){
-                let table = new DataTable('#myTable');
+                $('.btnHapus').click(function(){
+                    return confirm("Hapus data?");
+                })
             });
-        </script> --}}
+        </script>
     </body>
 </html>
